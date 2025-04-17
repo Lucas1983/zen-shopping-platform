d0a8f376ed8f0f4bd0a2db4a8b41b3e97e8dc7ab
Lukasz Mielczarek <mielczarek.lukasz.karol@gmail.com>
2025-04-17 07:39:36 +0200

    Rename ProductRetrieverService to ProductRetrievalService for clarity

--- src/main/java/com/zen/ala/application/service/ProductRetrievalService.java
+++ src/main/java/com/zen/ala/application/service/ProductRetrievalService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ProductRetrieverService implements ProductRetriever {
+public class ProductRetrievalService implements ProductRetriever {
 
   private final ProductLoader productLoader;
 
