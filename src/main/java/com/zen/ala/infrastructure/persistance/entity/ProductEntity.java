2ee36d0a4d1b3d33da53d5eec88559444e0d5466
Lukasz Mielczarek <mielczarek.lukasz.karol@gmail.com>
2025-04-17 18:32:02 +0200

    Updated ProductEntity by removing autogenerated ID

--- src/main/java/com/zen/ala/infrastructure/persistance/entity/ProductEntity.java
+++ src/main/java/com/zen/ala/infrastructure/persistance/entity/ProductEntity.java
@@ -3,10 +3,7 @@ package com.zen.ala.infrastructure.persistance.entity;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 /** ProductEntity represents a product in the database. */
 @Entity
@@ -17,11 +14,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ProductEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private UUID id;
+  @Id private UUID id;
 
   private String name;
-
   private BigDecimal price;
 }
