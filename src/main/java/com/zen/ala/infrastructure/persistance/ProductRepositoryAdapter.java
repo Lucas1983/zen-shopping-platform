299a4c214e18c76a1d46cf73630786933268ebec
Lukasz Mielczarek <mielczarek.lukasz.karol@gmail.com>
2025-04-17 21:30:05 +0200

    Refactor ProductRepositoryAdapter to implement ProductOutboundPort; add methods for finding, saving, updating, and deleting products with appropriate error handling

--- src/main/java/com/zen/ala/infrastructure/persistance/ProductRepositoryAdapter.java
+++ src/main/java/com/zen/ala/infrastructure/persistance/ProductRepositoryAdapter.java
@@ -1,25 +1,33 @@
 package com.zen.ala.infrastructure.persistance;
 
 import com.zen.ala.domain.model.Product;
-import com.zen.ala.domain.port.out.ProductLoader;
+import com.zen.ala.domain.port.out.ProductOutboundPort;
 import com.zen.ala.infrastructure.persistance.mapper.ProductEntityMapper;
 import com.zen.ala.infrastructure.persistance.repository.ProductRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 /**
- * ProductRepositoryAdapter is an adapter that implements the ProductLoader interface, and uses
- * ProductRepository for data access operations.
+ * Adapter class that implements the ProductOutboundPort interface and interacts with the
+ * ProductRepository to perform CRUD operations on Product entities.
  */
 @Component
 @RequiredArgsConstructor
-public class ProductRepositoryAdapter implements ProductLoader {
+public class ProductRepositoryAdapter implements ProductOutboundPort {
 
   private final ProductRepository productRepository;
   private final ProductEntityMapper productEntityMapper;
 
+  /**
+   * Finds a product by its ID.
+   *
+   * @param id the UUID of the product
+   * @return the Product object if found
+   * @throws IllegalArgumentException if the product is not found
+   */
   @Override
   public Product findProductById(UUID id) {
     return productRepository
@@ -28,16 +36,47 @@ public class ProductRepositoryAdapter implements ProductLoader {
         .orElseThrow(() -> new IllegalArgumentException("Product not found"));
   }
 
+  /**
+   * Finds all products.
+   *
+   * @return a list of Product objects
+   */
   @Override
   public List<Product> findAllProducts() {
     return productRepository.findAll().stream().map(productEntityMapper::toDomain).toList();
   }
 
+  /**
+   * Saves a new product.
+   *
+   * @param product the Product object to save
+   * @return the saved Product object
+   */
   @Override
-  public void saveProduct(Product product) {
-    productRepository.save(productEntityMapper.toEntity(product));
+  public Product saveProduct(Product product) {
+    return Optional.of(productRepository.save(productEntityMapper.toEntity(product)))
+        .map(productEntityMapper::toDomain)
+        .orElseThrow(() -> new IllegalArgumentException("Product not saved"));
   }
 
+  /**
+   * Updates an existing product.
+   *
+   * @param product the Product object to update
+   * @return the updated Product object
+   */
+  @Override
+  public Product updateProduct(Product product) {
+    return Optional.of(productRepository.save(productEntityMapper.toEntity(product)))
+        .map(productEntityMapper::toDomain)
+        .orElseThrow(() -> new IllegalArgumentException("Product not updated"));
+  }
+
+  /**
+   * Deletes a product by its ID.
+   *
+   * @param id the UUID of the product to delete
+   */
   @Override
   public void deleteProduct(UUID id) {
     productRepository.deleteById(id);
